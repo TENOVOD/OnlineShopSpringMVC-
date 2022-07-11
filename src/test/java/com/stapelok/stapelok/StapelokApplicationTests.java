8986b3162ebf8058ee02e279c35d686982d09b32
TENOVOD <karpenko2002in42@gmail.com>
2022-07-11 23:49:57 +0300

    First add

---
+++ src/test/java/com/stapelok/stapelok/StapelokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stapelok.stapelok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StapelokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
